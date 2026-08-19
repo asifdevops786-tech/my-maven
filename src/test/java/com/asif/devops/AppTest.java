f7470d0418726b6585b42c3a937ef4323fa36326
asifdevops786-tech <asifdevops786@gmail.commkdir>
2026-08-19 10:59:32 +0000

    apptest

--- src/test/java/com/asif/devops/AppTest.java
+++ src/test/java/com/asif/devops/AppTest.java
@@ -36,3 +36,14 @@ public class AppTest
         assertTrue( true );
     }
 }
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testHello() {
+        assertEquals("Hello", "Hello");
+    }
+}
+
